5b27172ab8b8174c382795981e30573379a06bde
yongcheol-1 <rladydcjf97@naver.com>
2025-01-21 00:33:30 +0900

    중간 저장

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,55 +1,30 @@
-'use client';
-
-import { useState } from 'react';
-import { useAuthStore } from '@/store';
-import { authApi } from '@/services/auth';
-import { useRouter } from 'next/navigation';
+// import Header from '@/components/common/Header';
+import Loginform from '../../../components/login/loginform';
+import Container from '@/components/login/container';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 export default function LoginPage() {
-  const router = useRouter();
-  const { login } = useAuthStore();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await authApi.login({
-        email,
-        password,
-      });
-
-      console.log('Login response:', response.data);
-
-      const { user, accessToken } = response.data;
-      login(user, accessToken);
-
-      router.push('/');
-    } catch (error) {
-      console.error('Login error:', error);
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div>로그인 테스트</div>
-      <div>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="이메일"
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호"
-        />
-      </div>
-      <button type="submit">로그인</button>
-    </form>
+    <div className="bg-gray-100 min-h-screen">
+      <Container
+        color="gray"
+        addClassName="max-w-[640px] max-h-[779px] w-full h-auto aspect-[640/779] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        <div className="flex flex-col gap-[56px]">
+          <Link href="/" aria-label="홈으로 이동">
+            <Image
+              src="/icons/logo_big.svg"
+              alt="노마드 로고"
+              width={340}
+              height={192}
+              className="block mx-auto"
+            />
+          </Link>
+          <Loginform />
+        </div>
+      </Container>
+    </div>
   );
 }
---
+++ src/components/login/loginform.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import Button from '../Button';
+import InputItem from './logininputitem';
+import Image from 'next/image';
+
+export default function SignInForm() {
+  return (
+    <div className="flex flex-col gap-[32px]">
+      <form className="gap-[32px] flex flex-col">
+        <InputItem
+          label="이메일"
+          id="email"
+          type="email"
+          placeholder="이메일을 입력해 주세요"
+        />
+        <InputItem
+          label="비밀번호"
+          id="pw"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+        />
+        <Button type="submit" color="primary">
+          로그인 하기
+        </Button>
+      </form>
+      <p className="flex gap-[10px] font-medium text-gray-900 text-[16px] mx-auto mt-8px">
+        회원이 아니신가요?
+        <Link href="/signup" aria-label="회원가입으로 이동">
+          <span className="text-gray-900 text-[16px] font-medium underline">
+            회원가입 하기
+          </span>
+        </Link>
+      </p>
+      <div className="flex gap-[40px] text-xl text-gray-800 font-regular flex justify-center items-center">
+        SNS 계정으로 로그인하기
+      </div>
+      <div className="flex gap-[16px] flex justify-center items-center ">
+        <Image
+          src="/icons/logo_google.svg"
+          alt="구글 로고"
+          width={72}
+          height={72}
+        />
+        <Image
+          src="/icons/logo_kakao.svg"
+          alt="카카오 로고"
+          width={72}
+          height={72}
+        />
+      </div>
+    </div>
+  );
+}
