80571c39543277b8a357333bf3725c4ee55184ff
swp91 <seongwoo4477@gmail.com>
2025-01-21 13:31:57 +0900

    사이드바 이미지 미리보기 작업

--- src/components/common/SideNavMenu.tsx
+++ src/components/common/SideNavMenu.tsx
@@ -1,20 +1,59 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
+import { useRef, useState, ChangeEvent } from 'react';
+
+interface ProfileImage {
+  file: File | null;
+  preview: string;
+}
 
 export default function SideNavMenu() {
-  // 메뉴 선택되는 상태변화를 segment로 하려고 생각중
+  // 메뉴 선택되는 상태변화를 segment로 추적하려고 생각중
   const segment = useSelectedLayoutSegment();
+
+  const [profileImage, setProfileImage] = useState<ProfileImage>({
+    file: null,
+    preview: '/icons/defaultuser_icon.svg',
+  });
+  const imageRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageURL = URL.createObjectURL(file);
+      setProfileImage({
+        file,
+        preview: imageURL,
+      });
+    }
+  };
+  const handleImage = () => {
+    imageRef.current?.click();
+  };
 
   return (
     <div className="w-[384px] h-[432px] rounded-xl p-6 flex flex-col items-center gap-6 border border-gray-300">
       <div className="w-40 h-40 relative">
-        <div className="w-full h-full rounded-full">
-          <Image src="/icons/defaultuser_icon.svg" fill alt="프로필사진" />
+        <div className="w-full h-full rounded-full overflow-hidden">
+          <img
+            src={profileImage.preview}
+            alt="프로필사진"
+            className="w-full h-full"
+          />
         </div>
-        <div className="w-11 h-11 rounded-full bg-green-3 absolute bottom-0 right-3 flex justify-center items-center cursor-pointer">
+        <input
+          type="file"
+          accept=".jpg,.jpeg,.png,.webp"
+          hidden
+          ref={imageRef}
+          onChange={handleFileChange}
+        />
+        <div
+          className="w-11 h-11 rounded-full bg-green-3 absolute bottom-0 right-3 flex justify-center items-center cursor-pointer"
+          onClick={handleImage}
+        >
           <svg width="24" height="24" viewBox="0 0 24 24">
             <path
               d="M16.8103 6.05983L4.05422 18.8473L3.28125 20.7186L5.1525 19.9456L17.94 7.18952L16.8103 6.05983ZM19.3627 3.50795L18.81 4.06014L19.9397 5.18983L20.4923 4.63717C20.6374 4.49205 20.7188 4.29529 20.7188 4.09014C20.7188 3.88499 20.6374 3.68823 20.4923 3.54311L20.4572 3.50795C20.3853 3.43608 20.3 3.37907 20.2061 3.34017C20.1122 3.30127 20.0116 3.28125 19.9099 3.28125C19.8083 3.28125 19.7076 3.30127 19.6137 3.34017C19.5198 3.37907 19.4345 3.43608 19.3627 3.50795Z"
