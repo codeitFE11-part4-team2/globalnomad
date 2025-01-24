4075d8ad7a2c412e30c818e672fd682d1191f70f
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2025-01-24 12:22:38 +0900

    feet: 배너의 서브 이미지 구현

---
+++ src/components/activitydetail/SubImage.tsx
@@ -0,0 +1,82 @@
+import { SubImage } from '@/lib/activitydetail/activitydetailTypes';
+import Image from 'next/image';
+
+interface SubImageBannerProps {
+  subImages: SubImage[];
+}
+
+const SubImageBanner = ({ subImages }: SubImageBannerProps) => {
+  const newSubImages = [];
+
+  // 서브 이미지가 없을 때
+  if (subImages.length === 0) {
+    return <div />;
+  }
+
+  // 서브 이미지가 1개일 때
+  if (subImages.length === 1) {
+    newSubImages.push(
+      <Image
+        key={subImages[0].id}
+        src={subImages[0].imageUrl}
+        alt="서브 이미지"
+      />
+    );
+    return <div>{newSubImages}</div>;
+  }
+
+  // 서브 이미지가 2개일 때
+  if (subImages.length === 2) {
+    for (let index = 0; index < 2; index += 1) {
+      newSubImages.push(
+        <Image
+          key={subImages[index].id}
+          src={subImages[index].imageUrl}
+          alt={`서브 이미지 ${index + 1}`}
+        />
+      );
+    }
+    return <div>{newSubImages}</div>;
+  }
+
+  // 서브 이미지가 3개일 때
+  if (subImages.length === 3) {
+    for (let index = 0; index < 3; index += 1) {
+      if (index === 2) {
+        newSubImages.push(
+          <Image
+            key={subImages[index].id}
+            src={subImages[index].imageUrl}
+            alt={`서브 이미지 ${index + 1}`}
+          />
+        );
+        break;
+      }
+      newSubImages.push(
+        <img
+          key={subImages[index].id}
+          src={subImages[index].imageUrl}
+          alt={`서브 이미지 ${index + 1}`}
+        />
+      );
+    }
+    return <div>{newSubImages}</div>;
+  }
+
+  // 서브 이미지가 4개일 때
+  if (subImages.length === 4) {
+    for (let index = 0; index < 4; index += 1) {
+      newSubImages.push(
+        <Image
+          key={subImages[index].id}
+          src={subImages[index].imageUrl}
+          alt={`서브 이미지 ${index + 1}`}
+        />
+      );
+    }
+  }
+
+  return <div>{newSubImages}</div>;
+};
+
+export default SubImageBanner;
