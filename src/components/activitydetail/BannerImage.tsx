0cafe2e86eb5631afee756894efcfea27b5c7eef
장수경 <jangsookyeong@jangsoogyeong-ui-MacBookAir.local>
2025-01-24 12:42:56 +0900

    feet: 배너 이미지 구현

--- src/components/activitydetail/BannerImage.tsx
+++ src/components/activitydetail/BannerImage.tsx
@@ -1,1 +1,26 @@
+import { SubImage } from '@/lib/activitydetail/activitydetailTypes';
+import Image from 'next/image';
+import React from 'react';
+import SubImageBanner from '@/components/activitydetail/SubImage';
 
+interface BannerImageProps {
+  bannerImages: string;
+  subImages: SubImage[];
+}
+
+const BannerImage = ({ bannerImages, subImages }: BannerImageProps) => {
+  return (
+    <div>
+      <div>
+        {subImages.length === 0 ? (
+          <Image src={bannerImages} alt="배너 이미지" />
+        ) : (
+          <Image src={bannerImages} alt="배너 이미지" />
+        )}
+        <SubImageBanner subImages={subImages} />
+      </div>
+    </div>
+  );
+};
+
+export default BannerImage;
