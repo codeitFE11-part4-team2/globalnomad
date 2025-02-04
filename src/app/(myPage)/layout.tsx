55a05e203f2ed55c083bd01e7969590f45b3fc75
swp91 <seongwoo4477@gmail.com>
2025-02-04 13:15:55 +0900

    현재 사용하지 않는 prop 정리

--- src/app/(myPage)/layout.tsx
+++ src/app/(myPage)/layout.tsx
@@ -1,17 +1,15 @@
 import { ReactNode } from 'react';
 import SideNavMenu from '@/app/(myPage)/_component/SideNavMenu';
 
-type Props = { children: ReactNode; modal: ReactNode };
-export default function myPageLayout({ children, modal }: Props) {
+type Props = { children: ReactNode };
+export default function myPageLayout({ children }: Props) {
   // 추후 로그인여부 판별하는 훅 붙일예정
-
   return (
     <div className="w-[344px] md:w-[696px] lg:w-[1208px] flex md:gap-4 lg:gap-6 justify-center mx-auto mt-6 lg:mt-[72px]">
       <div className="hidden md:block">
         <SideNavMenu />
       </div>
       <div className="flex-1">{children}</div>
-      {modal}
     </div>
   );
 }
