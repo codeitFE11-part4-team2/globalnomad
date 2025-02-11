ac2984bf8065377136099c8a8670e0e89450261c
swp91 <seongwoo4477@gmail.com>
2025-02-11 14:06:31 +0900

    카테고리 컴포넌트 분리

--- src/app/(myPage)/createactivity/_component/CreateActivityForm.tsx
+++ src/app/(myPage)/createactivity/_component/CreateActivityForm.tsx
@@ -8,6 +8,7 @@ import { useAuthStore } from '@/store';
 import AddressInput from './AddressInput';
 import ScheduleInput from './ScheduleInput';
 import ImageSelector from './ImageSelector';
+import CategorySelector from './CategorySelector';
 
 interface Schedule {
   id: number;
@@ -43,22 +44,7 @@ export default function CreateActivityForm() {
         }}
       />
 
-      <select
-        name="category"
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        className="w-full text-lg h-14 mt-6 border border-gray-800 rounded-md px-2 focus:outline-none text-gray-800 appearance-none bg-[url('/icons/selectArrow_icon.svg')] bg-no-repeat bg-right"
-      >
-        <option value="" disabled hidden>
-          카테고리
-        </option>
-        <option value="문화예술">문화예술</option>
-        <option value="식음료">식음료</option>
-        <option value="스포츠">스포츠</option>
-        <option value="투어">투어</option>
-        <option value="관광">관광</option>
-        <option value="웰빙">웰빙</option>
-      </select>
+      <CategorySelector category={category} setCategory={setCategory} />
 
       <textarea
         placeholder="설명"
