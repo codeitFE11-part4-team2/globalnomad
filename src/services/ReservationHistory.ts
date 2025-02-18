bd4b86654d36b233ae2bc1b7e342158469471c49
KimTaeHun <kth32105@naver.com>
2025-02-19 04:31:42 +0900

    feat: api 추가 및 수정

--- src/services/ReservationHistory.ts
+++ src/services/ReservationHistory.ts
@@ -1,15 +1,3 @@
-// import { ReservationResponse } from '@/lib/reservations/types';
-// import { mockReservationResponse } from '@/lib/reservations/mockData';
-
-// export async function fetchReservations(): Promise<ReservationResponse> {
-//   // mock 데이터를 사용하여 API 호출 시뮬레이션
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve(mockReservationResponse);
-//     }, 1000); // 1초 딜레이
-//   });
-// }
-
 import { api } from '@/lib/axios';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
