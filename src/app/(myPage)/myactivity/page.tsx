a12c4774b60c58e022518a6b7002bbebad2e5595
yongcheol-1 <rladydcjf97@naver.com>
2025-02-21 15:18:53 +0900

    SideNavMenu수정

--- src/app/(myPage)/myactivity/page.tsx
+++ src/app/(myPage)/myactivity/page.tsx
@@ -1,9 +1,14 @@
+'use client'; // 클라이언트 컴포넌트에서만 동작
+
+import { useRouter } from 'next/navigation';
 import { Button } from '@/components/common/Button';
 import MyActivityCard from './_component/MyActivityCard';
 import CardModal from './_component/CardModal';
 
 export default function Page() {
-  /**임시로 사용할 데이터 */
+  const router = useRouter(); // useRouter 훅 사용
+
+  /** 임시로 사용할 데이터 */
   const mockActivities = [
     {
       id: 1,
@@ -35,7 +40,9 @@ export default function Page() {
     <div className="flex flex-col gap-6">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold">내 체험 관리</h2>
-        <Button>체험 등록하기</Button>
+        <Button onClick={() => router.push('/createactivity')}>
+          체험 등록하기
+        </Button>
       </div>
       <div className="flex flex-col gap-4">
         {mockActivities.map((item) => (
