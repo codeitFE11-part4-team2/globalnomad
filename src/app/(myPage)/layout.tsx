9f283e3743adfbb15224a444beabbac68cd1e4e8
yongcheol-1 <rladydcjf97@naver.com>
2025-02-23 21:22:05 +0900

    빌드 확인

--- src/app/(myPage)/layout.tsx
+++ src/app/(myPage)/layout.tsx
@@ -1,26 +1,8 @@
-'use client';
-
 import { ReactNode } from 'react';
-import { usePathname } from 'next/navigation'; // 현재 경로를 확인하기 위한 훅
 import SideNavMenu from '@/app/(myPage)/_component/SideNavMenu';
-
-type LayoutProps = {
-  children: ReactNode;
-  modal?: ReactNode; // modal을 선택적 속성으로 추가
-};
-
-export default function myPageLayout({ children, modal }: LayoutProps) {
-  const pathname = usePathname(); // 현재 경로를 가져옵니다.
-
-  // '/mobilemyinform' 경로인 경우 레이아웃을 제외
-  if (pathname === '/mobilemyinform') {
-    return (
-      <>
-        {children} {/* /mobilemyinform일 때는 children만 렌더링 */}
-        {modal && modal} {/* modal이 있으면 렌더링 */}
-      </>
-    );
-  }
+type Props = { children: ReactNode; modal: ReactNode };
+export default function myPageLayout({ children, modal }: Props) {
+  // 추후 로그인여부 판별하는 훅 붙일예정
 
   return (
     <div className="w-full md:w-[696px] lg:w-[1200px] flex md:gap-4 lg:gap-6 justify-center mx-auto mt-6 lg:mt-[72px] px-6">
@@ -28,7 +10,7 @@ export default function myPageLayout({ children, modal }: LayoutProps) {
         <SideNavMenu />
       </div>
       <div className="flex-1">{children}</div>
-      {modal && modal} {/* modal이 있으면 렌더링 */}
+      {modal}
     </div>
   );
 }
