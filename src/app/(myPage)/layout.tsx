b44393de0b0ab27f8ce5ae191673816a7fa1ad41
yongcheol-1 <rladydcjf97@naver.com>
2025-02-23 21:43:48 +0900

    빌드 에러 수정

--- src/app/(myPage)/layout.tsx
+++ src/app/(myPage)/layout.tsx
@@ -1,16 +1,24 @@
+'use client';
+
 import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
 import SideNavMenu from '@/app/(myPage)/_component/SideNavMenu';
-type Props = { children: ReactNode; modal: ReactNode };
-export default function myPageLayout({ children, modal }: Props) {
-  // 추후 로그인여부 판별하는 훅 붙일예정
+
+type Props = { children: ReactNode };
+
+export default function myPageLayout({ children }: Props) {
+  const pathname = usePathname();
+
+  if (pathname === '/mobilemyinform') {
+    return <>{children}</>;
+  }
 
   return (
-    <div className="w-full md:w-[696px] lg:w-[1200px] flex md:gap-4 lg:gap-6 justify-center mx-auto mt-6 lg:mt-[72px] px-6">
+    <div className="w-[344px] md:w-[696px] lg:w-[1208px] flex md:gap-4 lg:gap-6 justify-center mx-auto mt-6 lg:mt-[72px]">
       <div className="hidden md:block">
         <SideNavMenu />
       </div>
       <div className="flex-1">{children}</div>
-      {modal}
     </div>
   );
 }
